363e788f5c61bce31150eda4d31afab31df1d4ac
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-03-30 12:37:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,11 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[]) {
-    std::cout << "MP3 Combiner Starting..." << std::endl;
-    
+int main() {
     AudioAnalyzer analyzer;
-    
-    // Set default values
-    analyzer.setInputSource("file", "input.mp3");
-    analyzer.setRepeaterCallsign("N0CALL");
-    
-    // Process command line arguments
-    std::string inputFile = (argc > 1) ? argv[1] : "input.mp3";
-    
-    // Run analysis
-    analyzer.analyze(inputFile);
-    
-    // Save results
-    analyzer.saveAnalysis("analysis_results.txt");
-    
-    std::cout << "Analysis complete!" << std::endl;
+    analyzer.analyzeAudio("test.mp3");
+    analyzer.setRepeaterCallsign("W1AW");
+    analyzer.setBroadcastifyCredentials("username", "password");
+    analyzer.downloadFromBroadcastify("12345", "00:00", "01:00");
     return 0;
 }
